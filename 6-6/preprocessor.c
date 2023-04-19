3c28759d26742b4ae8402d311515e87fb610fbdc
saher <msaher.shair@gmail.com>
2023-04-19 09:42:35 +0300

    added 6-6/preprocessor.c

---
+++ 6-6/preprocessor.c
@@ -0,0 +1,13 @@
+#include "hashtable.h"
+#include <stdio.h>
+#define getch() getc(stdin)
+#define ungetch(c) ungetc(c, stdin)
+
+int main(int argc, char* argv[])
+{
+    int c;
+    while((c = getch()) != EOF) {
+        
+    }
+    return 0;
+}
